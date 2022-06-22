d4bfbd2b8d9afc6d1cbdf825559cca903cfa6f1d
Brian Lapuste <seblapuste@gmail.com>
2022-06-22 16:20:56 -0400

    Add UserItems, ItemList, and styling

---
+++ frontend/src/user/components/UsersList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './UsersList.css';
+import UsersItem from './UserItem';
+
+const UsersList = props => {
+        if (props.items.length == 0) {
+            return (
+            <div className='center'>
+                <h2>No Users found.</h2>
+            </div>
+            );
+        };
+
+   
+        return <ul className='users-list'>
+            {props.items.map(user => (
+                <UsersItem
+                key={user.id}
+                id={user.id}
+                image={user.image}
+                name={user.name}
+                placeCount={user.places}
+                />
+            ))}
+        </ul>;
+};
+
+export default UsersList;
--- frontend/src/user/components/pages/Users.js
+++ frontend/src/user/components/pages/Users.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import UsersItem from '../UserItem';
+import UsersList from '../UsersList';
 
 const Users = () => {
-   return <h2>Users component is working</h2>;
+   const USERS = [
+               {
+                  id: 'u1',
+                  name: 'Brian Lapuste',
+                  image: 'https://media-exp1.licdn.com/dms/image/C5603AQFBGxWho1kryQ/profile-displayphoto-shrink_800_800/0/1653687712712?e=1661385600&v=beta&t=n5Yqb3WXQc3OfmhuA8IC3iG-z6g9VHuLcefUKZ7dzMs',
+                  places: 3
+               }
+            ];
+
+
+   return <UsersList items={USERS}/>;
 };
 
 export default Users;
